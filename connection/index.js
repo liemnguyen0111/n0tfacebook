ac30c0ceaeef2bac0a9b6166040ed6ca14c633de
Liem Nguyen <liemnguyen0111@gmail.com>
2020-06-08 22:30:04 -0700

    Updated index.js in connection

--- connection/index.js
+++ connection/index.js
@@ -1,6 +1,9 @@
 const Sequelize = require("sequelize");
 
- const sequelize = new Sequelize(process.env.JAWSDBM_URL);
+ const sequelize = new Sequelize(process.env.JAWSDBM_URL, 
+    {
+  dialect:  'mysql'
+    });
 
 // const sequelize = new Sequelize(process.env.LOCAL_URL);
 
