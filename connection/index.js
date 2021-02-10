d545b07a51aad16f3bf35e645fde33e6c7f261ba
Liem Nguyen <liemnguyen0111@gmail.com>
2021-02-10 13:37:55 -0800

    updated

--- connection/index.js
+++ connection/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 
- const sequelize = new Sequelize(process.env.JAWSDB_MARIA_BLUE_URL || "mysql://sg2zqgafsmi5yhhm:nnbou8wslifh9uyj@un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/t65bzvfr46dx4dat", 
+ const sequelize = new Sequelize(process.env.JAWSDB_MARIA_BLUE_URL,
     {
   dialect:  'mysql'
     });
